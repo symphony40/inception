145d5043895e1dcc92c09977e96b17a06c048c49
symphony40 <88945381+symphony40@users.noreply.github.com>
2024-12-20 15:31:49 +0100

    Update script.sh

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-ROOT_ENV_FILE="$(dirname "$0")/../../.env"
-
-# Load environment variables from .env file
-if [ -f "$ROOT_ENV_FILE" ]; then
-    set -o allexport
-    source "$ROOT_ENV_FILE"
-    set +o allexport
-else
-    echo "Root .env file not found at $ROOT_ENV_FILE!"
-    exit 1
-fi
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=admin@admin.com --allow-root
+./wp-cli.phar user create $WP_ADMIN_USR main@admin.com --role=admin --user_pass=$WP_ADMIN_PWD --allow-root
+./wp-cli.phar user create $WP_USR admin2@admin.com --role=author --user_pass=$WP_PWD --allow-root
 
 php-fpm8.2 -F
